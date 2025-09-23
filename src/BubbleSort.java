39c4910ef971c6d208bd6861676680c38ae283b3
HP <HP@STEVENCBR>
2025-09-23 07:51:24 -0500

    Estructuras

---
+++ src/BubbleSort.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    // Algoritmo Bubble Sort con impresión paso a paso
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (less(a[j + 1], a[j])) {
+                    exch(a, j, j + 1);
+                }
+            }
+            // Mostrar estado del arreglo después de cada pasada
+            System.out.print("Iteración " + (i + 1) + ": ");
+            show(a);
+        }
+        assert isSorted(a);
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static void show(Comparable[] a) {
+        for (Comparable elem : a) {
+            System.out.print(elem + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese elementos separados por espacio:");
+        String linea = sc.nextLine();
+        String[] datos = linea.split(" ");
+
+        System.out.println("Arreglo original:");
+        show(datos);
+
+        sort(datos);
+
+        System.out.println("Arreglo final ordenado:");
+        show(datos);
+
+        sc.close();
+    }
+}
